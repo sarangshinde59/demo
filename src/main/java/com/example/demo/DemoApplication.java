700ac417a1d781c4708db433452b923efe685fb5
sarangshinde59 <42888470+sarangshinde59@users.noreply.github.com>
2024-08-04 11:47:33 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,13 @@ public class DemoApplication {
 
 	 public static void main(String[] args) {
 		 int a;
+		 int b;
+		 int c;
+		 int d;
+		 int e;
+		 int f;
+		 int i;
+		 int j;
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
